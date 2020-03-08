a92f4a6d91f2e1ed5001a677aa5f45fc953eb1f9
rafaelaccampos <rafaela-cassiano@hotmail.com>
2020-03-08 10:34:31 -0300

    build: Initial project

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = { 
+    presets: [
+    "@babel/preset-env",
+    "@babel/preset-react"
+    ],
+};
+
+//@babel/presets-env serve para o navegador entender JS (import/export)
+//@babel/presets-react serve para o navegador entender React
